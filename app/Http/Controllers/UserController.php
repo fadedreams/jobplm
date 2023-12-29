0dbb42168f4ff5ed23dce1f2ac0195845d124e96
fadedreams7 <fadedreams7@gmail.com>
2023-12-29 01:19:10 -0500

    SRReq+

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
+use App\Http\Requests\SRReq;
 use App\Models\User;
 
 class UserController extends Controller
@@ -13,17 +13,20 @@ class UserController extends Controller
         return view('users.seeker-register');
     }
 
-    public function storeSeeker(Request $request)
+    // public function storeSeeker(Request $request)
+    public function storeSeeker(SRReq $request)
     {
+        // Check if there is an authorization check here
+        // $this->authorize('create', User::class);
         request()->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:1', 'confirmed'],
         ]);
         User::create([
             'name' => request('name'),
             'email' => request('email'),
-            'password' => request('password'),
+            'password' => bcrypt(request('password')),
             'user_type' => 'seeker',
         ]);
         return back();
--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
     //     'email',
     //     'password',
     // ];
+    const JOB_SEEKER = 'seeker';
     protected $guarded = [];
 
     /**
