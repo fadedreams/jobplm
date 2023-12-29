e5d7034f1e3996b49bdb4b839c5bcbf204818e25
fadedreams7 <fadedreams7@gmail.com>
2023-12-29 02:59:36 -0500

    dash middleware+

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,13 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//import Auth
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    // public function index()
+    // {
+    //     if (Auth::check()) {
+    //         return view('dashboard');
+    //     }
+    //     return back();
+    // }
     public function index()
     {
-        return view('dashboard');
-
-        // return $contact;
+        if (Auth::check()) {
+            return view('dashboard');
+        }
+        return back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::post('/login', [UserController::class, 'postLogin'])->name('login.post');
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+// Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
 
 
 
