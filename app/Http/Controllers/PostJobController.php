e1963146163d1c78e810bd3d5d2bf4bca079d97c
fadedreams7 <fadedreams7@gmail.com>
2023-12-31 11:20:36 -0500

    job post created

--- app/Http/Controllers/PostJobController.php
+++ app/Http/Controllers/PostJobController.php
@@ -3,25 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Middleware\isEmployer;
+use App\Http\Middleware\IsEmployer;
 use App\Http\Middleware\isPremiumUser;
 use App\Http\Requests\JobEditFormRequest;
-use App\Http\Requests\JobPostFormRequest;
 use App\Models\Listing;
 use App\Post\JobPost;
 use Illuminate\Support\Str;
+use App\Http\Requests\JPR;
 
 class PostJobController extends Controller
 {
 
     protected $job;
-    // public function __construct(JobPost $job)
-    // {
-    //     $this->job = $job;
-    //     $this->middleware('auth');
-    //     $this->middleware(isPremiumUser::class)->only(['create', 'store']);
-    //     $this->middleware(isEmployer::class);
-    // }
+    public function __construct(JobPost $job)
+    {
+        $this->job = $job;
+        $this->middleware('auth');
+        // $this->middleware(IsPremiumUser::class)->only(['create', 'store']);
+        $this->middleware(IsEmployer::class);
+    }
 
     public function index()
     {
@@ -35,27 +35,33 @@ class PostJobController extends Controller
         return view('job.create');
     }
 
-
-    public function store(Request $request)
+    public function store(JPR $request)
     {
-        $image_path = $request->file('feature_image')->store('public/images');
-
-        $post = new Listing;
-        $post->user_id = auth()->user()->id;
-        $post->feature_image = $image_path;
-        $post->title = $request->title;
-        $post->description = $request->description;
-        $post->job_type = $request->description;  // <-- This might be a typo, should it be $request->job_type?
-        $post->address = $request->address;
-        $post->application_close_date = $request->date;
-        $post->salary = $request->salary;
-        $post->slug = Str::slug($request->title) . '.' . Str::uuid();
-
-        $post->save();
-
+        $this->job->store($request);
         return back();
         // return redirect()->route('job.index')->with('success', 'Your job post has been posted');
     }
+
+    // public function store(JPR $request)
+    // {
+    //     $image_path = $request->file('feature_image')->store('public/images');
+    //
+    //     $post = new Listing;
+    //     $post->user_id = auth()->user()->id;
+    //     $post->feature_image = $image_path;
+    //     $post->title = $request->title;
+    //     $post->description = $request->description;
+    //     $post->job_type = $request->description;  // <-- This might be a typo, should it be $request->job_type?
+    //     $post->address = $request->address;
+    //     $post->application_close_date = $request->date;
+    //     $post->salary = $request->salary;
+    //     $post->slug = Str::slug($request->title) . '.' . Str::uuid();
+    //
+    //     $post->save();
+    //
+    //     return back();
+    //     // return redirect()->route('job.index')->with('success', 'Your job post has been posted');
+    // }
 
 
     // public function store(JobPostFormRequest $request)
